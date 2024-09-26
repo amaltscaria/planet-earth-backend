355e4d04395ba612f07145b9e6a2359f2d02d724
Amal T Scaria <amaltscaria22@gmail.com>
2024-09-26 11:40:01 +0530

    Add gitignore file

--- server.js
+++ server.js
@@ -1,34 +1,41 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import fs from 'fs';
-import path from 'path';
-import cors from 'cors';
+import express from "express";
+import bodyParser from "body-parser";
+import fs from "fs";
+import path from "path";
+import cors from "cors";
+import dotenv from "dotenv";
+import { fileURLToPath } from "url";
+dotenv.config();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
-const PORT = 3000;
-
+const PORT = process.env.PORT;
 // Middleware to enable CORS
-app.use(cors({
-    origin: 'http://localhost:5173', // Allow access from your frontend
-  }));
+app.use(
+  cors({
+    origin: process.env.FRONT_END_URL, // Allow access from your frontend
+  })
+);
 
 // Middleware to parse JSON bodies
-app.use(bodyParser.json({ limit: '10mb' }));
+app.use(bodyParser.json({ limit: "10mb" }));
 // Serve static files from the 'dist' folder
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, "dist")));
 
 // Serve static files from the uploads directory
-app.use('/uploads', express.static(path.join(process.cwd(), 'uploads')));
+app.use("/uploads", express.static(path.join(process.cwd(), "uploads")));
 
 // Endpoint to upload the certificate
-app.post('/api/upload-certificate', (req, res) => {
+app.post("/api/upload-certificate", (req, res) => {
   const imgData = req.body.image;
 
   // Extract the base64 part
-  const base64Data = imgData.replace(/^data:image\/jpeg;base64,/, '');
+  const base64Data = imgData.replace(/^data:image\/jpeg;base64,/, "");
 
   // Create uploads directory if it doesn't exist
-  const uploadsDir = path.join(process.cwd(), 'uploads');
+  const uploadsDir = path.join(process.cwd(), "uploads");
   if (!fs.existsSync(uploadsDir)) {
     fs.mkdirSync(uploadsDir);
   }
@@ -38,14 +45,14 @@ app.post('/api/upload-certificate', (req, res) => {
   const filePath = path.join(uploadsDir, fileName);
 
   // Write the file
-  fs.writeFile(filePath, base64Data, 'base64', (err) => {
+  fs.writeFile(filePath, base64Data, "base64", (err) => {
     if (err) {
-      console.error('Error saving the image:', err);
-      return res.status(500).send('Error saving the image');
+      console.error("Error saving the image:", err);
+      return res.status(500).send("Error saving the image");
     }
 
     // Send back the file path
-    const fileUrl = `https://${req.get('host')}/uploads/${fileName}`;
+    const fileUrl = `https://${req.get("host")}/uploads/${fileName}`;
     res.json({ filePath: fileUrl });
   });
 });
